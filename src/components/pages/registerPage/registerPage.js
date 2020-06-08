2d18a29b940273c6a25f76e56cc439028859e639
moris <moris.rafol@gmail.com>
2020-06-08 18:06:46 +0300

    feat: tweak register page

--- src/components/pages/registerPage/registerPage.js
+++ src/components/pages/registerPage/registerPage.js
@@ -2,29 +2,39 @@ import React from 'react'
 import InputFieldWrapper from "../../common/inputFieldWrapper/inputFieldWrapper";
 import TextField from "../../common/textField/textField";
 import "./registerPage.css"
-
+import "../../../index.css"
 
 export function RegisterPage() {
 
 
-
     return <div className={"register"}>
-        <img src={"/images/img-logo-512.png"} alt={"logo"} className={"register_logo"} />
-        <h1>like minds</h1>
-        <h4>Helping to connect like-minded personalities</h4>
 
-            <div className={"register_inputFields"}>
-                <InputFieldWrapper>
-                    <TextField title={"Email"} inputType={"Email"} placeHolder={"Enter your email..."} />
-                </InputFieldWrapper>
-                <InputFieldWrapper>
-                    <TextField title={"password"} inputType={"password"} placeHolder={"Enter your password..."}/>
+        <div className={"register_logoContainer"}>
+            <img src={"/images/img-logo-512.png"} alt={"logo"} className={"register_logo"}/>
+            <h1>like minds</h1>
+            <h4>Helping to connect like-minded personalities</h4>
+        </div>
 
-                </InputFieldWrapper>
+        <form className={"register_inputFields"}>
+            <InputFieldWrapper>
+                <TextField title={"Email"} inputType={"Email"} placeHolder={"Enter your email..."}/>
+            </InputFieldWrapper>
+            <InputFieldWrapper>
+                <TextField title={"password"} inputType={"password"} placeHolder={"Enter your password..."}/>
+            </InputFieldWrapper>
 
-            </div>
+            <input type="button" value={"Login"} className={"blackBtn roundedCorners_lite register_button"}/>
+            <input type="button" value={"Register"} className={"blackBtn roundedCorners_lite register_button"}/>
+
+            <hr/>
+
+            <input type="button" value={"Sign in with Google"}
+                   className={"orangeBtn roundedCorners_lite register_button"}/>
+
+        </form>
 
 
     </div>
+
 
 }
