8e63265bd39b8bc18c701a9f0482229603c14b83
James Daniels <jrd656@gmail.com>
2020-06-08 15:06:05 +0100

    just did userProfileInut compenent initial work

---
+++ src/components/common/profileInputField/profileInputField.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./profileInputField.css"
+const { useState } = React;
+
+function profileInputField({ canEdit, onChange
+    , content, title, placeHolder, rowCount }) {
+
+    // const [content, setContent] = useState("")
+
+    return (
+        <div className='textField' >
+<p>Hey</p>
+        </div>
+    );
+}
+
+export default profileInputField;
