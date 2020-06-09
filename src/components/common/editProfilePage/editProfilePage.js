0905c95501518d49a40a8b5cfc6eb95538b5a0a1
James Daniels <jrd656@gmail.com>
2020-06-09 11:01:35 +0100

    moris is lagging

--- src/components/common/editProfilePage/editProfilePage.js
+++ src/components/common/editProfilePage/editProfilePage.js
@@ -1,8 +1,9 @@
+import ProfileInputField from "../profileInputField/profileInputField";
+import ProfileGallery from "../profileGallery/profileGallery"
 import React from "react";
 // import "./";
 const { useState } = React;
-import ProfileInputField from "../profileInputField/profileInputField";
-import ProfileGallery from "../profileGallery/profileGallery"
+
 
 function EditProfilePage({ canEdit, onChange, imagesArr }) {
     // const [content, setContent] = useState("")
