804f3e1e4f5ca979b255fee4789df90861230f8a
James Daniels <jrd656@gmail.com>
2020-06-10 11:15:07 +0100

    message previews done

--- src/App.js
+++ src/App.js
@@ -10,27 +10,15 @@ import {
 //pages----------------------------------------------------------------------------------
 import AuthRoutes from "./components/routes/authRoutes";
 import ProfileRoutes from "./components/routes/profileRoutes";
-import MatchPopup from "./components/common/matchPopup/matchPopup"
-
-
+import Messages from './components/pages/messages/messages'
 
 
 import {
      useRecoilValue,
 } from 'recoil';
-<<<<<<< HEAD
-import "./tools/recoil/recoilStates"
-import {LoginPage} from "./components/pages/loginPage/loginPage";
-import ProfileInputField from "./components/common/profileInputField/profileInputField";
-import ProfileGallery from "./components/common/profileGallery/profileGallery"
-import EditProfilePage from "./components/pages/editProfilePage/editProfilePage"
-import Messages from "./components/pages/messages/messages"
-
-// import {reduce} from 'immer'
-=======
 import {loggedInState} from "./tools/recoil/recoilStates";
 import NavBar from "./components/common/navBar/navBar";
->>>>>>> master
+
 
 
 
@@ -38,42 +26,12 @@ function App() {
     const isLoggedIn = useRecoilValue(loggedInState)
 
     return (
-<<<<<<< HEAD
-        <RecoilRoot>
-            <div>
-                <Switch>
-                    <Route exact  path="/login/successful"  >
-                        test
-                    </Route>
-                    <Route exact  path="/login">
-                        <LoginPage />
-                    </Route>
-
-                    <Route path="/editProfile">
-                        <EditProfilePage />
-                    </Route>
-
-                    <Route path="/messages">
-                        <Messages />
-                    </Route>
-
-                    <Route  exact  path="/">
-                        home
-                    </Route>
-
-
-                </Switch>
-            </div>
-        </RecoilRoot>
-=======
         <div>
             {isLoggedIn? <NavBar/>: null }
             <AuthRoutes/>
             <ProfileRoutes/>
-            <MatchPopup/>
-
+            <Messages/>
         </div>
->>>>>>> master
     );
 }
 
--- src/components/common/MessagesPreviews/MessagesPreviews.js
+++ src/components/common/MessagesPreviews/MessagesPreviews.js
@@ -1,31 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import "./MessagesPreviews.css";
 const { useState } = React;
 
 
-const data= [ { profile: { id: 2, userid: 2, firstname: 'Moris', lastname: 'Rafoul', gender: 'male', status: 'Single', bio: 'Teaching web-dev', job: 'Web-Developer', livingin: 'Haifa', primaryphoto: 'https://avatars0.githubusercontent.com/u/10247681?s=60&v=4', subphotos: [ 'https://avatars2.githubusercontent.com/u/51966598?s=60&v=4', 'https://avatars0.githubusercontent.com/u/10247681?s=60&v=4', 'https://avatars0.githubusercontent.com/u/10247681?s=60&v=4' ] }, lastMessage:  { id: 2, senderUserId: 1, recipUserId: 2, message: 'How\'s tricks?', timeAndDate: new Date('Fri May 01 2020 13:08:29 GMT+0300 (Israel Daylight Time)')} } ]
+const data = [{ profile: { id: 2, userid: 2, firstname: 'Moris', lastname: 'Rafoul', gender: 'male', status: 'Single', bio: 'Teaching web-dev', job: 'Web-Developer', livingin: 'Haifa', primaryphoto: 'https://avatars0.githubusercontent.com/u/10247681?s=60&v=4', subphotos: ['https://avatars2.githubusercontent.com/u/51966598?s=60&v=4', 'https://avatars0.githubusercontent.com/u/10247681?s=60&v=4', 'https://avatars0.githubusercontent.com/u/10247681?s=60&v=4'] }, lastMessage: { id: 2, senderUserId: 1, recipUserId: 2, message: 'How\'s tricks?', timeAndDate: new Date('Fri May 01 2020 13:08:29 GMT+0300 (Israel Daylight Time)') } }]
 
+const defaultImage = "/images/defaultImageProfile.png";
 
-function MessagesPreviews({}) {
-
-
+function MessagesPreviews({ }) {
 
     return (
-        <div className="messagesWrapper">
-          <div>
-                <img></img>
+        <Fragment>
+            <p id="pending">pending (1)</p>
 
-            </div>
-            <div>
-                <p>Nme of Candidate</p>
-                <p>Last message received</p>
-                </div>
-
+            <div className="messagePreviews">
                 <div>
-                    <p>New or Not?</p>
-                    <p>Time Sent </p>
+                    <img src={defaultImage}></img>
                 </div>
-        </div>
+                <div className="nameAndMessage">
+                    <p id="candidateName">Paul Gascoigne</p>
+                    <p id="messagePreview">Hey, how's it going?</p>
+                </div>
+
+                <div className="newAndTime">
+                    <p id="new">New</p>
+                    <p id="timeSince">5 minutes ago</p>
+                </div>
+            </div>
+        </Fragment>
     );
 }
 
