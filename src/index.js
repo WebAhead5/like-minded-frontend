b4bccef346a58d11872f7664ed46b5ee830f0b38
moris <moris.rafol@gmail.com>
2020-06-14 03:15:05 +0300

    fix: update dependencies

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import {Router} from "react-router-dom";
 import './index.css';
 import App from './App';
-import {RecoilRoot} from "recoil"
 import {history} from "./tools/history";
 
 ReactDOM.render(
     <Router history={history}>
-        <RecoilRoot>
             <App/>
-        </RecoilRoot>
     </Router>
 
     ,
